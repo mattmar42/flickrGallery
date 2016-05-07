808850321480f841979a16c7e90428ccfa989e8c
Matt <matthewmaring42@gmail.com>
2016-05-07 17:04:46 -0400

    2nd comit, fixed a bug stopping the search from working.

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 $(document).ready(function(){
 $('#newSearch').hide();
 
-$('#input').submit(function(){
-		var search = $('#search').val();
-		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+ $('form').submit(function (evt) {
+ 	evt.preventDefault();
+	var search = $('#search').val();
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
 	  {
 	    tags: search,
 	    format: "json"
 	  },
-	  function(data) {
+	function(data) {
 	  	$('#images').empty()
-	    $.each(data.items, function(i,item){
-	      $("<img />").attr("src", item.media.m).appendTo("#images");
-	      $('#newSearch').show();
-	    });//ajax function end
-	  });//ajax end
+	    $.each(data.items, function(i,photo){
+	    $("<img />").attr("src", photo.media.m).appendTo("#images");
+	    
+  });//ajax function end
+ });//ajax end
 });//on submit end
 
-$('#newSearch').click(function(){
-
-	$('#images').empty();
-
-});
 
 });// document ready end
