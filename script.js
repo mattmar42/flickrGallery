b61f9264854e1fb63ac1fe19209cbe3e09d97c14
Matt <matthewmaring42@gmail.com>
2016-05-10 20:31:53 -0400

    fixed a bug adding too many images

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ $('#newSearch').hide();
 	var search = $('#search').val();
 	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
 	  {
+	  	method: 'flickr.photosets.getphotos',
+	  	per_page: '12',
 	    tags: search,
-	    format: "json"
+	    format: "json",
 	  },
 	  //Removes any currently posted images in case of a second+ search, for each item parsed it creates an image attribute and appends it to the div with an id of 'images'
 	function(data) {
-	  	$('#images').empty()
-	    $.each(data.items, function(i,photo){
-	    $("<img />").attr("src", photo.media.m).appendTo("#images");
-	    
-  });//ajax function end
+		var images = $('#images');
+	  	$('#images').empty();
+	  	$(images).empty();
+		    $.each(data.items, function(i,item){
+				$("<img />").attr("src", item.media.m).appendTo("#images");
+				if ( i === 14 ) return false;
+	});//ajax function end
  });//ajax end
 });//on submit end
 
